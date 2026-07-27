66492836a2a0a9f9283571af758417ba24864f88
Satyam Kumar <rroy60110@gmail.com>
2026-07-27 18:03:48 +0530

    Updated code

--- 01_basics/02_datatypes-summary.js
+++ 01_basics/02_datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 93429428483842492n
 
@@ -27,10 +27,40 @@ let myObj = {
 
 
 const myFunction = function(){
-    console.log("hello world");
+    // console.log("hello world");
 }
  
 console.log(typeof outsideTemp);
+
+
+// __________________________________________________________________________________
+
+
+// Heap Memory(Permitive) , Stack Memory(Non Permitive)
+
+let myYoutubename = "satyam kumar"
+
+let anothername = myYoutubename
+anothername = "toon world"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let userOne ={
+     email : "user@google.com",
+     upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+   userTwo.email =  "satyam@google.com"
+
+   console.log(userOne.email);
+   console.log(userTwo.email);
+   
+
+
 
 
 
