c4081d5199bb3f575670a6ad13549b8608264e87
Satyam Kumar <rroy60110@gmail.com>
2026-07-27 15:05:06 +0530

    Your commit message

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+ const accountID = 167388
+ let accountEmail = "satyam@google.com"
+ var accountPassword = "123456"
+ accountCity = "Delhi"
+
+//  accountID = 2
+
+accountEmail = "peluu@google.com"
+accountPassword = "76327"
+accountCity = "patna"
+
+ console.log(accountID);
+ 
+ console.table([accountEmail,accountCity,accountPassword]);
---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+let score= 33
+
+// console.log (typeof score);
+
+
+// ***************************OPERATION*************************
+
+let value=3
+let negValue= -value
+console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+
+ 
+let str1= "Hello"
+let str2= " Satyam"
+
+let str3 =str1+str2;
+
+console.log(str3);
