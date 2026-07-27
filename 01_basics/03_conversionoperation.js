1792b001ef27ce275aea423dc1101303b0e4ed35
Satyam Kumar <rroy60110@gmail.com>
2026-07-27 15:19:57 +0530

    update code

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -7,7 +7,7 @@ let score= 33
 
 let value=3
 let negValue= -value
-console.log(negValue);
+// console.log(negValue);
 
 
 // console.log(2+2);
@@ -23,4 +23,13 @@ let str2= " Satyam"
 
 let str3 =str1+str2;
 
-console.log(str3);
+// console.log(str3);
+
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
