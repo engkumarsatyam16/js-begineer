c59e1cb4b6804d5686ae9de4f1a3e246e96ab362
Satyam Kumar <rroy60110@gmail.com>
2026-07-27 16:30:37 +0530

    update code

---
+++ 01_basics/02_datatypes-summary.js
@@ -0,0 +1,37 @@
+// permitive
+
+// 7 types = string, number, boolean, null, undefined, BigInt, symbol
+
+const score=100
+const scoreValue= 100.4
+
+const isloggedIn = false
+const outsideTemp = null 
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 93429428483842492n
+
+// Refrence (Non Permitive)= Arrray , object, function
+
+const heros = ["Shaktiman", "naagraaj", "doga"]
+
+let myObj = {
+    name: "satyam",
+    age: 21,
+}
+
+
+const myFunction = function(){
+    console.log("hello world");
+}
+ 
+console.log(typeof outsideTemp);
+
+
+
+
